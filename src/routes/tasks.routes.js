0e1486aa0d2dde3dba04bd6126eba2dc1007db98
nicolassso <nicolasleal1996@gmail.com>
2021-12-21 13:29:07 +0100

    creating a full MERN project, connected to mongoDB, with the help of FAZT

---
+++ src/routes/tasks.routes.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const router = express.Router()
+
+const Task = require('../models/tasks')
+
+router.get('/', async (req, res) => {
+    const tasks = await Task.find()
+    res.json(tasks)
+})
+
+router.post('/', async (req, res) => {
+    const { title, description } = req.body
+    const task = new Task({
+        title, 
+        description
+    })
+    await task.save()
+    res.json({
+        status: 'Task saved'
+    })
+})
+
+module.exports = router
