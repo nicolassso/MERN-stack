294e72b7b7c5575b67ed45cca6e08a384c665cad
nicolassso <nicolasleal1996@gmail.com>
2021-12-22 00:18:56 +0100

    added all routes for CRUD

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -8,6 +8,11 @@ router.get('/', async (req, res) => {
     res.json(tasks)
 })
 
+router.get('/:id', async (req, res) => {
+    const task = await Task.findById(req.params.id)
+    res.json(task)
+})
+
 router.post('/', async (req, res) => {
     const { title, description } = req.body
     const task = new Task({
@@ -18,6 +23,18 @@ router.post('/', async (req, res) => {
     res.json({
         status: 'Task saved'
     })
+})
+
+router.put('/:id', async (req, res) => {
+    const { title, description } = req.body
+    const newTask = { title, description }
+    await Task.findByIdAndUpdate(req.params.id, newTask)
+    res.json({status: "Task updated"})
+})
+
+router.delete('/:id', async (req, res) => {
+    await Task.findByIdAndRemove(req.params.id)
+    res.json({status: "task deleted"})
 })
 
 module.exports = router
